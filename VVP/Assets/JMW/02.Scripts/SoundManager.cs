2cc18594d2b6ba1b19a1fa064f0045cf10799080
minuk <cosmot0@naver.com>
2021-06-07 09:16:04 +0900

    .

---
+++ VVP/Assets/JMW/02.Scripts/SoundManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    //public AudioClip audioClipPong;
+    public AudioClip gameBG;
+    //public AudioClip rankingBG;
+    //public AudioClip audioClipApplause;
+    //public AudioClip potion;
+    //public AudioClip finish;
+    //public AudioClip portalSound;
+    //public AudioClip panpareSound;
+    //public AudioClip titleBG;
+    //public AudioClip mainBG;
+
+
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        if (SoundManager.instance == null)
+        {
+            SoundManager.instance = this;
+        }
+    }
+
+    //public void PlayPong()
+    //{
+    //    audioSource.PlayOneShot(audioClipPong);
+
+
+
+    //}
+
+    public void GameBGM()
+    {
+        audioSource.PlayOneShot(gameBG);
+
+
+
+    }
+
+    //public void TitleBGM()
+    //{
+    //    audioSource.PlayOneShot(titleBG);
+
+
+
+    //}
+
+
+    //public void MainBGM()
+    //{
+    //    audioSource.PlayOneShot(mainBG);
+
+
+
+    //}
+
+    //public void RankingBGM()
+    //{
+    //    audioSource.PlayOneShot(rankingBG);
+    //}
+
+
+    //public void ApplauseSound()
+
+    //{
+    //    audioSource.PlayOneShot(audioClipApplause);
+    //}
+
+
+    //public void potionSound()
+    //{
+    //    audioSource.PlayOneShot(potion);
+    //}
+
+
+    //public void finished()
+    //{
+    //    audioSource.PlayOneShot(finish);
+    //}
+
+    //public void portalSnd()
+    //{
+    //    audioSource.PlayOneShot(portalSound);
+    //}
+
+    //public void pangpare()
+    //{
+    //    audioSource.PlayOneShot(panpareSound);
+    //}
+
+
+}
