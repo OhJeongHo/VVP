91fbd250e2809d160240be665d066dc5315fc975
minuk <cosmot0@naver.com>
2021-06-07 11:41:35 +0900

    .

--- VVP/Assets/JMW/02.Scripts/LaserFire.cs
+++ VVP/Assets/JMW/02.Scripts/LaserFire.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class LaserFire : MonoBehaviour
 {
     LineRenderer lr;
+
+    //GameObject character;
     
     void Start()
     {
         lr = GetComponent<LineRenderer>();
         lr.startColor = new Color(0.222f, 0, 1, 0.4f);
 
+        //Camera c = character.GetComponent<Camera>();
+        //c.enabled = false;
     }
 
     
-    void Update()
+    void LateUpdate()
     {
 
         //// 1번키를 누르면
