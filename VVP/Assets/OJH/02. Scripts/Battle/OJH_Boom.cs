f0801e98a5c61fb1de184361e1072f4fb46d0ad9
minuk <cosmot0@naver.com>
2021-06-08 14:41:05 +0900

    Update OJH_Boom.cs

--- VVP/Assets/OJH/02. Scripts/Battle/OJH_Boom.cs
+++ VVP/Assets/OJH/02. Scripts/Battle/OJH_Boom.cs
@@ -18,6 +18,7 @@ public class OJH_Boom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameManager.instance.isVR) return;
         // colls = Physics.OverlapSphere(transform.position, 2f);
         foreach (Collider coll in colls)
         {
