56573a4bf27e33958a82ccb23066babf20056c93
OhJeongHo <ojh5015@gmail.com>
2021-06-11 15:20:25 +0900

    .

---
+++ VVP/Assets/OJH/02. Scripts/Battle/OJH_CreateRocket.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OJH_CreateRocket : MonoBehaviourPun
+{
+    float currTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currTime += Time.deltaTime;
+
+        if (currTime > 20)
+        {
+            float newX = Random.Range(-27f, 32f), newZ = Random.Range(-2.4f, 37f);
+            PhotonNetwork.Instantiate("Rocket", new Vector3(newX, gameObject.transform.position.y, newZ), Quaternion.Euler(-90, 0, 0));
+            // transform.position = new Vector3(newX, gameObject.transform.position.y, newZ);
+            float newX2 = Random.Range(-27f, 32f), newZ2 = Random.Range(-2.4f, 37f);
+            PhotonNetwork.Instantiate("Rocket", new Vector3(newX2, gameObject.transform.position.y, newZ2), Quaternion.Euler(-90, 0, 0));
+            // 포톤으로 연료 생성
+            currTime = 0;
+        }
+    }
+}
